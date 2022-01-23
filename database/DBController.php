29d7508d8a496dac1277a16d603dfbded056beaf
cengicilma <ilma.cengic@gmail.com>
2022-01-23 22:24:09 +0100

    heroku db fix1

--- database/DBController.php
+++ database/DBController.php
@@ -4,10 +4,10 @@
 class DBController
 {
     // Database Connection Properties
-    //protected $host = 'localhost:3307';
-    //protected $user = 'ilma';
-    //protected $password = 'ilma';
-    //protected $database = "shopee";
+    protected $host = 'eu-cdbr-west-02.cleardb.net';
+    protected $user = 'bb318d7035ad99';
+    protected $password = '860606f2';
+    protected $database = "heroku_5320e771ab01e00";
 
     // connection property
     public $con = null;
@@ -16,19 +16,20 @@ class DBController
     public function __construct()
     {
         //Get Heroku ClearDB connection information
-        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-        $cleardb_server = $cleardb_url["host"];
-        $cleardb_username = $cleardb_url["user"];
-        $cleardb_password = $cleardb_url["pass"];
-        $cleardb_db = substr($cleardb_url["path"],1);
-        $active_group = 'default';
-        $query_builder = TRUE;
+        //$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        //$cleardb_server = $cleardb_url["host"];
+        //$cleardb_username = $cleardb_url["user"];
+        //$cleardb_password = $cleardb_url["pass"];
+        //$cleardb_db = substr($cleardb_url["path"],1);
+
+        //$active_group = 'default';
+        //$query_builder = TRUE;
 
         // Connect to DB
-        $this->con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+        //$this->con = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 
 
-        //$this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
+        $this->con = mysqli_connect($this->host, $this->user, $this->password, $this->database);
         if ($this->con->connect_error){
             echo "Fail " . $this->con->connect_error;
         }
